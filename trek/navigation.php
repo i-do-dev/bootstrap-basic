45243cc64c7ceaad23be0a4575a999b21d234e85
Asim Sarwar <asim.softwareengg@gmail.com>
2023-06-20 03:20:59 -0700

    Add course nav.

--- trek/navigation.php
+++ trek/navigation.php
@@ -12,6 +12,10 @@
       <img src="<?php echo $treks_src; ?>/assets/img/<?php echo $post->post_name === "treks" || $post->post_type === "tl_trek" ? "nav_treks_selected.svg" : "nav_Treks.svg"; ?>" />
       <a href="<?php echo site_url("treks") ?>">TREKs</a>
     </li>
+    <li class="<?php echo $post->post_name === "courses" || $post->post_type === "tl_course" ? "nav-section-selected" : ""; ?>">
+      <img src="<?php echo $treks_src; ?>/assets/img/<?php echo $post->post_name === "courses" || $post->post_type === "tl_course" ? "nav_treks_selected.svg" : "nav_Treks.svg"; ?>" />
+      <a href="<?php echo site_url("courses") ?>">Courses</a>
+    </li>
     <li class="<?php echo $post->post_name === "calendar" ? "nav-section-selected" : ""; ?>">
       <img src="<?php echo $treks_src; ?>/assets/img/calendar<?php echo $post->post_name === "calendar" ? "-selected" : ""; ?>.svg" />
       <a href="<?php echo site_url("calendar") ?>">Calendar</a>
